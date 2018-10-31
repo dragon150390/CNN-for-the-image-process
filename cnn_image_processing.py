fea4c8f4076a99901a3621c2f6db613509bf6797
dragon150390 <jamebond.nguyen@gmail.com>
2018-10-31 21:22:44 +0700

    The simple start for CNN

---
+++ cnn_image_processing.py
@@ -0,0 +1,9 @@
+#Import matplotlib
+import matplotlib.pyplot as plt
+
+# Load data from image
+data = plt.imread('c:/Users/thinkpad/Documents/bricks.png')
+
+#Display Image
+plt.imshow(data)
+plt.show()
