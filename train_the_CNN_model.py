42bd000125e5fa6550e5c04c355a01172a57bb7f
silverlightweb <jamebond.nguyen@gmail.com>
2018-12-03 12:11:36 +0700

    Train a deep CNN to classify clothing images Training a deep learning model Training a deep learning model is very similar to training a single layer network. Once the model is constructed (as you have done in the previous exercise), the model needs to be compiled with the right set of parameters. Then, the model is fit by providing it with training data, as well as training labels. After training is done, the model can be evaluated on test data

---
+++ train_the_CNN_model.py
@@ -0,0 +1,14 @@
+from keras.models import Sequential
+
+# Compile model
+model.compile(optimizer='Adam', 
+              loss='categorical_crossentropy', 
+              metrics=['accuracy'])
+
+# Fit the model to training data 
+model.fit(train_data, train_labels, 
+          validation_split=0.2, 
+          epochs=3, batch_size=10)
+
+# Evaluate the model on test data
+model.evaluate(train_data, train_labels, batch_size=10)
